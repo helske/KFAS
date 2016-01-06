df3ae79b04d8187ea64d34714f076b423f84a717
Jouni Helske <jouni.helske@jyu.fi>
2016-01-06 09:19:05 +0200

    some double quote changes

--- R/predict.SSModel.R
+++ R/predict.SSModel.R
@@ -53,12 +53,12 @@
 #' model <- SSModel(y~x,distribution="poisson")
 #' xnew <- seq(0.5,3.5,by=0.1)
 #' newdata <- SSModel(rep(NA,length(xnew))~xnew,distribution="poisson")
-#' pred <- predict(model,newdata=newdata,interval='prediction',level=0.9,nsim=100)
+#' pred <- predict(model,newdata=newdata,interval="prediction",level=0.9,nsim=100)
 #' plot(x=x,y=y,pch=19,ylim=c(0,25),xlim=c(0.5,3.5))
-#' matlines(x=xnew,y=pred,col=c(2,2,2),lty=c(1,2,2),type='l')
+#' matlines(x=xnew,y=pred,col=c(2,2,2),lty=c(1,2,2),type="l")
 #' 
 #' model <- SSModel(Nile~SSMtrend(1,Q=1469),H=15099)
-#' pred <- predict(model,n.ahead=10,interval='prediction',level=0.9)
+#' pred <- predict(model,n.ahead=10,interval="prediction",level=0.9)
 #' pred
 predict.SSModel <- function(object, newdata, n.ahead, 
                             interval = c("none", "confidence", "prediction"), level = 0.95, 
