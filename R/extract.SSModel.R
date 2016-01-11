f1b3367a9b5e912245b8e3f2f49972dc11afc006
Jouni Helske <jouni.helske@jyu.fi>
2016-01-11 08:48:43 +0200

    extract SSModel components with numeric values

--- R/extract.SSModel.R
+++ R/extract.SSModel.R
@@ -43,7 +43,7 @@
 #' logLik(model, check.model = TRUE) #with check.model = FALSE R crashes!
 `[<-.SSModel` <-  function(x, element, states, etas, series, times, ...,
   value) {
-
+  
   element <- match.arg(arg = element,
     choices = c("y", "Z", "H", "T", "R", "Q", "a1", "P1",
       "P1inf", "u"))
@@ -61,7 +61,7 @@
           choices = c("all", "arima", "custom", "cycle",
             "seasonal", "trend", "level", "slope", "regression"),
           several.ok = TRUE)
-
+        
         if ("all" %in% states) {
           states <- 1:attr(x, "m")
         } else {
@@ -86,7 +86,7 @@
           choices = c("all", "arima", "custom", "cycle",
             "seasonal", "trend", "level", "slope", "regression"),
           several.ok = TRUE)
-
+        
         if ("all" %in% etas) {
           etas <- 1:attr(x, "k")
         } else {
@@ -126,94 +126,100 @@
       P1inf = x[[element]][states, states] <- value, )
   }
   x
+  
 }
 #' @export
 #' @rdname Extract.SSModel
 `[.SSModel` <-
-  function(x, element, states, etas, series, times, drop=TRUE,...) {
-
-    element <- match.arg(arg = element,
-      choices = c("y", "Z", "H", "T", "R", "Q", "a1", "P1",
-        "P1inf", "u"))
-
-    if (!(element %in% c("y", "u", "Q"))) {
-      if (missing(states)) {
-        states <- 1:attr(x, "m")
-      } else {
-        if (is.numeric(states)) {
-          states <- as.integer(states)
-          if (min(states) < 1 | max(states) > attr(x, "m"))
-            stop("Vector states should contain the indices or types of the
-                 states which are modified.")
-        } else {
-          states <- match.arg(arg = states,
-            choices = c("all", "arima", "custom", "cycle",
-              "seasonal", "trend", "level", "slope","regression"),
-            several.ok = TRUE)
-
-          if ("all" %in% states) {
-            states <- 1:attr(x, "m")
-          } else {
-            if("trend" %in% states)
-              states <- c(states, "level", "slope")
-            states <- which(attr(x, "state_types") %in% states)
-          }
-        }
-      }
-    }
-    if (element %in% c("R", "Q")) {
-      if (missing(etas)) {
-        etas <- 1:attr(x, "k")
-      } else {
-        if (is.numeric(etas)) {
-          etas <- as.integer(etas)
-          if (min(etas) < 1 | max(etas) > attr(x, "k"))
-            stop("Vector etas should contain the indices or types of the etas
-                 which are modified.")
-        } else {
-          etas <- match.arg(arg = etas,
-            choices = c("all", "arima", "custom", "cycle",
-              "seasonal", "trend", "level", "slope", "regression"),
-            several.ok = TRUE)
-
-          if ("all" %in% etas) {
-            etas <- 1:attr(x, "k")
-          } else {
-            if("trend" %in% etas)
-              etas <- c(etas, "level", "slope")
-            etas <- which(attr(x, "eta_types") %in% etas)
-          }
-        }
-      }
-    }
-    if (element %in% c("y", "u", "Z")) {
-      if (missing(series)) {
-        series <- 1:attr(x, "p")
-      } else if (!all(series %in% (1:attr(x, "p"))))
-        stop("Argument series must have values between 1 to p, where p is the number of time series in model. ")
-    }
-    if (missing(times)) {
-      switch(element, y = ,
-        u = x[[element]][, series, drop = drop],
-        Z = x[[element]][series, states, , drop = drop],
-        H = x[[element]][series, series, , drop = drop],
-        T = x[[element]][states, states, , drop = drop],
-        R = x[[element]][states, etas, , drop = drop],
-        Q = x[[element]][etas, etas, , drop = drop],
-        a1 = x[[element]][states, 1, drop = drop],
-        P1 = x[[element]][states, states, drop = drop],
-        P1inf = x[[element]][states, states, drop = drop],
-      )
+  function(x, element, states, etas, series, times, drop=TRUE, ...) {
+    
+    if (is.numeric(element)) {
+      unclass(x)[element]
     } else {
-      switch(element, y = ,
-        u = x[[element]][times, series, drop = drop],
-        Z = x[[element]][series, states, times, drop = drop],
-        H = x[[element]][series, series, times, drop = drop],
-        T = x[[element]][states, states, times, drop = drop],
-        R = x[[element]][states, etas, times, drop = drop],
-        Q = x[[element]][etas, etas, times, drop = drop],
-        a1 = x[[element]][states, 1, drop = drop],
-        P1 = x[[element]][states, states, drop = drop],
-        P1inf = x[[element]][states, states, drop = drop], )
+      
+      element <- match.arg(arg = element,
+        choices = c("y", "Z", "H", "T", "R", "Q", "a1", "P1",
+          "P1inf", "u"))
+      
+      if (!(element %in% c("y", "u", "Q"))) {
+        if (missing(states)) {
+          states <- 1:attr(x, "m")
+        } else {
+          if (is.numeric(states)) {
+            states <- as.integer(states)
+            if (min(states) < 1 | max(states) > attr(x, "m"))
+              stop("Vector states should contain the indices or types of the
+                 states which are modified.")
+          } else {
+            states <- match.arg(arg = states,
+              choices = c("all", "arima", "custom", "cycle",
+                "seasonal", "trend", "level", "slope","regression"),
+              several.ok = TRUE)
+            
+            if ("all" %in% states) {
+              states <- 1:attr(x, "m")
+            } else {
+              if("trend" %in% states)
+                states <- c(states, "level", "slope")
+              states <- which(attr(x, "state_types") %in% states)
+            }
+          }
+        }
+      }
+      if (element %in% c("R", "Q")) {
+        if (missing(etas)) {
+          etas <- 1:attr(x, "k")
+        } else {
+          if (is.numeric(etas)) {
+            etas <- as.integer(etas)
+            if (min(etas) < 1 | max(etas) > attr(x, "k"))
+              stop("Vector etas should contain the indices or types of the etas
+                 which are modified.")
+          } else {
+            etas <- match.arg(arg = etas,
+              choices = c("all", "arima", "custom", "cycle",
+                "seasonal", "trend", "level", "slope", "regression"),
+              several.ok = TRUE)
+            
+            if ("all" %in% etas) {
+              etas <- 1:attr(x, "k")
+            } else {
+              if("trend" %in% etas)
+                etas <- c(etas, "level", "slope")
+              etas <- which(attr(x, "eta_types") %in% etas)
+            }
+          }
+        }
+      }
+      if (element %in% c("y", "u", "Z")) {
+        if (missing(series)) {
+          series <- 1:attr(x, "p")
+        } else if (!all(series %in% (1:attr(x, "p"))))
+          stop("Argument series must have values between 1 to p, where p is the number of time series in model. ")
+      }
+      if (missing(times)) {
+        switch(element, y = ,
+          u = x[[element]][, series, drop = drop],
+          Z = x[[element]][series, states, , drop = drop],
+          H = x[[element]][series, series, , drop = drop],
+          T = x[[element]][states, states, , drop = drop],
+          R = x[[element]][states, etas, , drop = drop],
+          Q = x[[element]][etas, etas, , drop = drop],
+          a1 = x[[element]][states, 1, drop = drop],
+          P1 = x[[element]][states, states, drop = drop],
+          P1inf = x[[element]][states, states, drop = drop],
+        )
+      } else {
+        switch(element, y = ,
+          u = x[[element]][times, series, drop = drop],
+          Z = x[[element]][series, states, times, drop = drop],
+          H = x[[element]][series, series, times, drop = drop],
+          T = x[[element]][states, states, times, drop = drop],
+          R = x[[element]][states, etas, times, drop = drop],
+          Q = x[[element]][etas, etas, times, drop = drop],
+          a1 = x[[element]][states, 1, drop = drop],
+          P1 = x[[element]][states, states, drop = drop],
+          P1inf = x[[element]][states, states, drop = drop], )
+      }
     }
   }
