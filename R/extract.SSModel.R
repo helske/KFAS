a63335dc0aefed53a63a1502eddbf2b911bc7c14
helske <jouni.helske@jyu.fi>
2016-01-11 09:50:55 +0200

    extraction and assignment via numeric values

--- R/extract.SSModel.R
+++ R/extract.SSModel.R
@@ -9,6 +9,8 @@
 #' @param x Object of class \code{SSModel}.
 #' @param element Which element is chosen. Possible choices are \code{"y"}, \code{"Z"}, \code{"H"},
 #'   \code{"T"}, \code{"R"}, \code{"Q"}, \code{"a1"}, \code{"P1"}, \code{"P1inf"}, and \code{"u"}.
+#'   For other values of \code{element}, default list extraction and assignments are used
+#'   (which do not check that the model components are of proper form).
 #' @param states Which states are chosen. Either a numeric vector containing the indices of the
 #'   states, or a character vector defining the types of the states. Possible choices are
 #'   \code{"all"},  \code{"level"}, \code{"slope"},
@@ -43,104 +45,110 @@
 #' logLik(model, check.model = TRUE) #with check.model = FALSE R crashes!
 `[<-.SSModel` <-  function(x, element, states, etas, series, times, ...,
   value) {
-  
-  element <- match.arg(arg = element,
+
+  tmp <- try(element <- match.arg(arg = element,
     choices = c("y", "Z", "H", "T", "R", "Q", "a1", "P1",
-      "P1inf", "u"))
-  if (!(element %in% c("y", "u", "Q"))) {
-    if (missing(states)) {
-      states <- 1:attr(x, "m")
-    } else {
-      if (is.numeric(states)) {
-        states <- as.integer(states)
-        if (min(states) < 1 | max(states) > attr(x, "m"))
-          stop("Vector states should contain the indices or names of the
-                 states which are modified.")
-      } else {
-        states <- match.arg(arg = states,
-          choices = c("all", "arima", "custom", "cycle",
-            "seasonal", "trend", "level", "slope", "regression"),
-          several.ok = TRUE)
-        
-        if ("all" %in% states) {
-          states <- 1:attr(x, "m")
-        } else {
-          if ("trend" %in% states)
-            states <- c(states, "level", "slope")
-          states <- which(attr(x, "state_types") %in% states)
-        }
-      }
-    }
-  }
-  if (element %in% c("R", "Q")) {
-    if (missing(etas)) {
-      etas <- 1:attr(x, "k")
-    } else {
-      if (is.numeric(etas)) {
-        etas <- as.integer(etas)
-        if (min(etas) < 1 | max(etas) > attr(x, "k"))
-          stop("Vector etas should contain the indices or names of the etas
-                 which are modified.")
-      } else {
-        etas <- match.arg(arg = etas,
-          choices = c("all", "arima", "custom", "cycle",
-            "seasonal", "trend", "level", "slope", "regression"),
-          several.ok = TRUE)
-        
-        if ("all" %in% etas) {
-          etas <- 1:attr(x, "k")
-        } else {
-          if ("trend" %in% etas)
-            etas <- c(etas, "level", "slope")
-          etas <- which(attr(x, "eta_types") %in% etas)
-        }
-      }
-    }
-  }
-  if (element %in% c("y", "u", "Z")) {
-    if (missing(series)) {
-      series <- 1:attr(x, "p")
-    } else if (!all(series %in% (1:attr(x, "p"))))
-      stop("Argument series must have values between 1 to p, where p is the
-             number of time series in model. ")
-  }
-  if (missing(times)) {
-    switch(element, y = , u = x[[element]][, series] <- value,
-      Z = x[[element]][series, states, ] <- value,
-      H = x[[element]][series, series, ] <- value,
-      T = x[[element]][states, states, ] <- value,
-      R = x[[element]][states, etas, ] <- value,
-      Q = x[[element]][etas, etas, ] <- value,
-      a1 = x[[element]][states, 1] <- value,
-      P1 = x[[element]][states, states] <- value,
-      P1inf = x[[element]][states, states] <- value, )
+      "P1inf", "u")), silent = TRUE)
+
+  if (inherits(tmp, "try-error")) {
+    class(x) <- "list"
+    x[element, ...] <- value
+    class(x) <- "SSModel"
   } else {
-    switch(element, y = , u = x[[element]][times, series] <- value,
-      Z = x[[element]][series, states, times] <- value,
-      H = x[[element]][series, series, times] <- value,
-      T = x[[element]][states, states, times] <- value,
-      R = x[[element]][states, etas, times] <- value,
-      Q = x[[element]][etas, etas, times] <- value,
-      a1 = x[[element]][states, 1] <- value,
-      P1 = x[[element]][states, states] <- value,
-      P1inf = x[[element]][states, states] <- value, )
+    if (!(element %in% c("y", "u", "Q"))) {
+      if (missing(states)) {
+        states <- 1:attr(x, "m")
+      } else {
+        if (is.numeric(states)) {
+          states <- as.integer(states)
+          if (min(states) < 1 | max(states) > attr(x, "m"))
+            stop("Vector states should contain the indices or names of the
+                 states which are modified.")
+        } else {
+          states <- match.arg(arg = states,
+            choices = c("all", "arima", "custom", "cycle",
+              "seasonal", "trend", "level", "slope", "regression"),
+            several.ok = TRUE)
+
+          if ("all" %in% states) {
+            states <- 1:attr(x, "m")
+          } else {
+            if ("trend" %in% states)
+              states <- c(states, "level", "slope")
+            states <- which(attr(x, "state_types") %in% states)
+          }
+        }
+      }
+    }
+    if (element %in% c("R", "Q")) {
+      if (missing(etas)) {
+        etas <- 1:attr(x, "k")
+      } else {
+        if (is.numeric(etas)) {
+          etas <- as.integer(etas)
+          if (min(etas) < 1 | max(etas) > attr(x, "k"))
+            stop("Vector etas should contain the indices or names of the etas
+                 which are modified.")
+        } else {
+          etas <- match.arg(arg = etas,
+            choices = c("all", "arima", "custom", "cycle",
+              "seasonal", "trend", "level", "slope", "regression"),
+            several.ok = TRUE)
+
+          if ("all" %in% etas) {
+            etas <- 1:attr(x, "k")
+          } else {
+            if ("trend" %in% etas)
+              etas <- c(etas, "level", "slope")
+            etas <- which(attr(x, "eta_types") %in% etas)
+          }
+        }
+      }
+    }
+    if (element %in% c("y", "u", "Z")) {
+      if (missing(series)) {
+        series <- 1:attr(x, "p")
+      } else if (!all(series %in% (1:attr(x, "p"))))
+        stop("Argument series must have values between 1 to p, where p is the
+             number of time series in model. ")
+    }
+    if (missing(times)) {
+      switch(element, y = , u = x[[element]][, series] <- value,
+        Z = x[[element]][series, states, ] <- value,
+        H = x[[element]][series, series, ] <- value,
+        T = x[[element]][states, states, ] <- value,
+        R = x[[element]][states, etas, ] <- value,
+        Q = x[[element]][etas, etas, ] <- value,
+        a1 = x[[element]][states, 1] <- value,
+        P1 = x[[element]][states, states] <- value,
+        P1inf = x[[element]][states, states] <- value, )
+    } else {
+      switch(element, y = , u = x[[element]][times, series] <- value,
+        Z = x[[element]][series, states, times] <- value,
+        H = x[[element]][series, series, times] <- value,
+        T = x[[element]][states, states, times] <- value,
+        R = x[[element]][states, etas, times] <- value,
+        Q = x[[element]][etas, etas, times] <- value,
+        a1 = x[[element]][states, 1] <- value,
+        P1 = x[[element]][states, states] <- value,
+        P1inf = x[[element]][states, states] <- value, )
+    }
   }
   x
-  
+
 }
 #' @export
 #' @rdname Extract.SSModel
 `[.SSModel` <-
-  function(x, element, states, etas, series, times, drop=TRUE, ...) {
-    
-    if (is.numeric(element)) {
-      unclass(x)[element]
+  function(x, element, states, etas, series, times, drop = TRUE, ...) {
+
+    tmp <- try(element <- match.arg(arg = element,
+      choices = c("y", "Z", "H", "T", "R", "Q", "a1", "P1",
+        "P1inf", "u")), silent = TRUE)
+    if (inherits(tmp, "try-error")) {
+      unclass(x)[element, drop = drop, ...]
     } else {
-      
-      element <- match.arg(arg = element,
-        choices = c("y", "Z", "H", "T", "R", "Q", "a1", "P1",
-          "P1inf", "u"))
-      
+
       if (!(element %in% c("y", "u", "Q"))) {
         if (missing(states)) {
           states <- 1:attr(x, "m")
@@ -155,11 +163,11 @@
               choices = c("all", "arima", "custom", "cycle",
                 "seasonal", "trend", "level", "slope","regression"),
               several.ok = TRUE)
-            
+
             if ("all" %in% states) {
               states <- 1:attr(x, "m")
             } else {
-              if("trend" %in% states)
+              if ("trend" %in% states)
                 states <- c(states, "level", "slope")
               states <- which(attr(x, "state_types") %in% states)
             }
@@ -180,11 +188,11 @@
               choices = c("all", "arima", "custom", "cycle",
                 "seasonal", "trend", "level", "slope", "regression"),
               several.ok = TRUE)
-            
+
             if ("all" %in% etas) {
               etas <- 1:attr(x, "k")
             } else {
-              if("trend" %in% etas)
+              if ("trend" %in% etas)
                 etas <- c(etas, "level", "slope")
               etas <- which(attr(x, "eta_types") %in% etas)
             }
@@ -223,3 +231,4 @@
       }
     }
   }
+
