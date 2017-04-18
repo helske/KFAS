13d84126eb2de12823671f2066e019b0f8746bbd
Jouni Helske <jouni.helske@jyu.fi>
2017-04-18 16:07:07 +0100

    to CRAN

--- R/mvInnovations.R
+++ R/mvInnovations.R
@@ -9,6 +9,23 @@
 #'}{v[t,i] = y[t] - Z[t]a[t]}}
 #'\item{F}{Prediction error variances \eqn{Var(v_{t})}{Var(v[t])}. }
 #'\item{Finf}{Diffuse part of \eqn{F_t}{F[t]}.}
+#'@examples
+#'
+#' # Compute the filtered estimates based on the KFS output
+#' 
+#' filtered <- function(x) {
+#'   innov <- mvInnovations(x)
+#'   att <- window(x$a, end = end(x$a) - 1)
+#'   tvz <- attr(x$model,"tv")[1]
+#'   
+#'   for (i in 1:nrow(att)) {
+#'     att[i,] <- att[i,] + 
+#'       x$P[,,i] %*% 
+#'       t(solve(innov$F[,,i], x$model$Z[, , tvz * (i - 1) + 1, drop = FALSE])) %*%
+#'       innov$v[i, ]
+#'   }
+#'   att
+#' }
 mvInnovations <- function(x){
   
   # Compute the multivariate versions of one-step-ahead prediction errors and variances
